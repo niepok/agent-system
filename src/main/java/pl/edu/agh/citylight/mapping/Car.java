767fde5b9dc07cd1adef2610b052e3e36c08e74a
Paweł Wamej <pwamej@gmail.com>
2017-05-17 15:21:17 +0200

    add agent fields in Car and StreetLight

--- src/main/java/pl/edu/agh/citylight/mapping/Car.java
+++ src/main/java/pl/edu/agh/citylight/mapping/Car.java
@@ -3,8 +3,19 @@ package pl.edu.agh.citylight.mapping;
 import org.jxmapviewer.JXMapViewer;
 import org.jxmapviewer.viewer.DefaultWaypoint;
 import org.jxmapviewer.viewer.GeoPosition;
+import pl.edu.agh.citylight.agents.CarAgent;
 
 public class Car extends Waypoint2D {
+    private CarAgent agent;
+
+    public CarAgent getAgent() {
+        return agent;
+    }
+
+    public void setAgent(CarAgent agent) {
+        this.agent = agent;
+    }
+
     Car(GeoPosition position, JXMapViewer mapViewer) {
         super(position, mapViewer);
     }
